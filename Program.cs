424f0798d769145682cc98332faa53cc8c34f919
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-08 06:33:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace OrderingDataUsingLINQ
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<string> { "Deer", "Sheep", "Cat", "Dog", "Bull","Donkey","Camel", "Emu", "Goat"};
+
+            foreach (var animal in animals.OrderBy(animal => animal))
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("________________\n");
+
+            foreach(var animal in animals.OrderByDescending(animal => animal))
+            {
+                Console.WriteLine(animal);
+            }
+
+            Console.WriteLine("________________\n");
+
+            var persons = new List<Person>
+            {
+                new Person("Nicholas", "Mamau"),
+                new Person("Ken", "Njuguna"),
+                new Person("Ken", "Njenga"),
+                new Person("Paul", "Kiprotech"),
+                new Person("Isaac", "Soita"),
+                new Person("James", "Kimani")
+            };
+
+            foreach (var person in persons.OrderBy(person => person.FirstName).ThenBy(person => person.LastName))
+            {
+                Console.WriteLine( $"{person.FirstName} {person.LastName}");
+            }
+
+            Console.WriteLine("________________\n");
+
+            animals.Reverse();
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+            record Person(string FirstName, string LastName);
+    }
+}
